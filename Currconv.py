b133aedf0de189de5a43ab4ea1f4cd8e010a1269
AmbSheep <creamghost@mail.ru>
2020-07-04 22:04:45 +0300

    updates

--- Currconv.py
+++ Currconv.py
@@ -1,15 +1,17 @@
+#!usr/bin/env python3
 import sys
 import requests
-import json
 
-base_currency = str(sys.argv[1])
-to = str(sys.argv[2])
-amount = float(sys.argv[3])
 
-url = 'https://api.exchangeratesapi.io/latest?base=' + base_currency
-request = requests.get(url)
-text = request.text
-data = json.loads(text)
-to_rate = data["rates"][to]  # find needed currency in the dictionary
-convert = float(amount * to_rate)
-print(convert)
+def convert(base, to, amount):
+    url = 'https://api.exchangeratesapi.io/latest?base=' + base
+    data = requests.get(url).json()
+    to_rate = data["rates"][to]  # find required currency in the dictionary
+    return amount * to_rate
+
+
+if __name__ == "__main__":
+    base = str(sys.argv[1])
+    to = str(sys.argv[2])
+    amount = float(sys.argv[3])
+    print(convert(base, to, amount))
